b16360b4a3f4e69c4d5067d1961a03c86e9368cf
aleanon <104034507+aleanon@users.noreply.github.com>
2024-11-13 16:49:31 +0100

    Project 1 C#

---
+++ emne 3/Project1/Board.cs
@@ -0,0 +1,9 @@
+namespace Project1;
+
+public class Board
+{
+   public static string NumToString(int number)
+   {
+      return number == 0 ? "" : Convert.ToString(number);
+   } 
+}
---
+++ emne 3/Project1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var quit = false;
+
+while (!quit)
+{
+    Console.SetCursorPosition(0,0);
+    
+    int[] numbers= [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    
+    
+    Console
+        .Write(
+             "---------------------------\n"
+            + "|        |        |        |\n"
+            + $"|   {numbers[0]}    |   {numbers[1]}    |    {numbers[2]}   |\n" 
+            + "|        |        |        |\n" 
+            + "---------------------------\n"
+            + "|        |        |        |\n" 
+            + $"|   {numbers[3]}    |    {numbers[4]}   |    {numbers[5]}   |\n" 
+            + "|        |        |        |\n" 
+            + "---------------------------\n"
+            + "|        |        |        |\n" 
+            + $"|   {numbers[6]}    |    {numbers[7]}   |    {numbers[8]}   |\n" 
+            + "|        |        |        |\n" 
+            + "---------------------------\n"
+        );
+    var command = Console.ReadLine();
+    if (command == "move right")
+    {
+        numbers[1] = 0;
+        numbers[0] = 1;
+    }
+}
+
